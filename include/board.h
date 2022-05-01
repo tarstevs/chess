80780b13f13126fd9423338471961d869c30c0f5
tarstevs <tarstevs@gmail.com>
2022-05-01 04:23:14 -0700

    made alg not let and num class var

--- include/board.h
+++ include/board.h
@@ -13,20 +13,19 @@ public:
 private:
   sf::Vector2u windowSize;
   sf::RenderTexture render_texture;
-
   sf::Color white_square = sf::Color(254, 232, 209);
   sf::Color black_square = sf::Color(83, 120, 99);
   sf::Color window_background_color = sf::Color(169, 169, 169);
   sf::Font free_sarif_font;
   sf::Font open_sans_font;
+  std::map<int, std::string> alg_notation_letter_map;
+  std::map<int, int> alg_notation_number_map;
   void render_algebraic_notation(int i, int j, float x_offset, float square_size);
-  template<typename T>
-  auto func(T obj, int i, int j, float x_offset, float square_size);
-  static std::string get_letter(int j);
-  static int get_number(int i);
   void render_piece(float x_offset, float square_size, int i, int j);
   void set_free_sarif_font();
   void set_open_sans_font();
+  void set_alg_notation_letter_map();
+  void set_alg_notation_number_map();
   void init_();
   void render_square(float x_offset, float square_size, int i, int j);
 };
--- src/board.cpp
+++ src/board.cpp
@@ -13,16 +13,6 @@ board::board(sf::Event &event) {
   init_();
 }
 
-std::string board::get_letter(int j) {
-  std::map<int, std::string> m_let{{0, "a"}, {1, "b"}, {2, "c"}, {3, "d"}, {4, "e"}, {5, "f"}, {6, "g"}, {7, "h"}};
-  return m_let.at(j);
-}
-
-int board::get_number(int i) {
-  std::map<int, int> m_num{{0, 8}, {1, 7}, {2, 6}, {3, 5}, {4, 4}, {5, 3}, {6, 2}, {7, 1}};
-  return m_num.at(i);
-}
-
 void board::render_board() {
 
   const float x_offset = static_cast<float>(windowSize.x - windowSize.y) / 2.f;
@@ -63,7 +53,7 @@ void board::render_piece(const float x_offset, const float square_size, int i, i
 void board::render_algebraic_notation(int i, int j, float x_offset, float square_size) {
 
   if (i == 7) {
-    sf::Text text(get_letter(j), open_sans_font, std::floor(square_size / 3.f));
+    sf::Text text(alg_notation_letter_map.at(j), open_sans_font, std::floor(square_size / 3.f));
     auto letter_color = j % 2 == 0 ? sf::Color(254, 232, 209) : sf::Color(83, 120, 99);
     text.setFillColor(letter_color);
     text.setPosition(square_size / 1.35f + x_offset + static_cast<float>(j) * square_size,
@@ -74,7 +64,7 @@ void board::render_algebraic_notation(int i, int j, float x_offset, float square
   }
 
   if (j == 0) {
-    sf::Text text(std::to_string(get_number(i)), open_sans_font, std::floor(square_size / 3.f));
+    sf::Text text(std::to_string(alg_notation_number_map.at(i)), open_sans_font, std::floor(square_size / 3.f));
     auto letter_color = i % 2 == 1 ? sf::Color(254, 232, 209) : sf::Color(83, 120, 99);
     text.setFillColor(letter_color);
     text.setPosition(square_size / 18.f + x_offset + static_cast<float>(j) * square_size,
@@ -84,6 +74,17 @@ void board::render_algebraic_notation(int i, int j, float x_offset, float square
     render_texture.display();
   }
 
+}
+
+void board::drawBoard(sf::RenderWindow &render_window) const {
+  render_window.draw(sf::Sprite(render_texture.getTexture()));
+}
+
+void board::init_() {
+  set_free_sarif_font();
+  set_open_sans_font();
+  set_alg_notation_letter_map();
+  set_alg_notation_number_map();
 }
 
 void board::set_free_sarif_font() {
@@ -97,11 +98,10 @@ void board::set_open_sans_font() {
   }
 }
 
-void board::drawBoard(sf::RenderWindow &render_window) const {
-  render_window.draw(sf::Sprite(render_texture.getTexture()));
+void board::set_alg_notation_letter_map() {
+  alg_notation_letter_map = {{0, "a"}, {1, "b"}, {2, "c"}, {3, "d"}, {4, "e"}, {5, "f"}, {6, "g"}, {7, "h"}};
 }
 
-void board::init_() {
-  set_free_sarif_font();
-  set_open_sans_font();
+void board::set_alg_notation_number_map() {
+  alg_notation_number_map = {{0, 8}, {1, 7}, {2, 6}, {3, 5}, {4, 4}, {5, 3}, {6, 2}, {7, 1}};
 }
